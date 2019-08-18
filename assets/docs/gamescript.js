5dd225e245a05c2b3142624dd6a0b2d56ffeba45
quething <m.getsoian@gmail.com>
2019-08-17 17:46:11 -0700

    baseline html

--- assets/docs/gamescript.js
+++ assets/docs/gamescript.js
@@ -16,12 +16,14 @@ let wins = 0; // number of questions beaten
     // begin timer
 
 // function: populate container
+function populateQuestionBox(){
     // get object at questionIndex
     // get question, print to question <p>
     // get wrong answers, print to wrong answer divs
     // get right answer, print to right answer divs
     // call shuffle function
-    // call startTimer functino
+    // call startTimer
+}
 
 // function: timer expires
     // store right answer as var
@@ -31,16 +33,20 @@ let wins = 0; // number of questions beaten
     // iterate losses
 
 // function: onclick correct answer
+$(".rightAnswer").on("click", function() {
     // stop timer
     // hide question div
     // show alert div
     // wins++
     // divtext to "That's right! Your score is " + wins
+})
 
 // function: onclick wrong answer
+$(".wrongAnswer").on("click", function() {
     // stop timer
     // store right answer as var
     // hide question div
     // show alert div
     // losses--
     // divtext to "Sorry, the correct answer was " + answer
+})
