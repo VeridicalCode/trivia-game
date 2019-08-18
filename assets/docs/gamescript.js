84d42e7d3360690f2d18b6fe9f278563803872c0
quething <m.getsoian@gmail.com>
2019-08-17 18:22:35 -0700

    test questions

--- assets/docs/gamescript.js
+++ assets/docs/gamescript.js
@@ -4,6 +4,29 @@ let questionIndex = 0; // start with the first question & iterate
 let losses = 0; // number of questions missed
 let wins = 0; // number of questions beaten
 
+let q1 = {
+    question: 'Which of the following people has never been in a Star Trek episode?',
+    correct: 'Winona Ryder',
+    fake1: 'King Abdullah II of Jordan',
+    fake2: 'Dwayne "The Rock" Johnson',
+    fake3: 'Stephen Hawking'
+}
+
+let q2 = {
+    question: 'What song did Zefram Cochrane listen to while conducting his warp drive test flight?',
+    correct: 'Steppenwolf’s "Magic Carpet Ride"',
+    fake1: 'Elton John’s "Rocket Man"',
+    fake2: 'David Bowie’s "Space Oddity"',
+    fake3: 'The Beastie Boys’s "Intergalactic"'
+}
+
+let q3 = {
+    question: 'In the DS9 episode "Far Beyond the Stars," Ben Sisko experiences another life in:',
+    correct: '1950s New York',
+    fake1: '2020s San Francisco',
+    fake2: '1970s Detroit',
+    fake3: '1840s Johannesburg'
+}
 
 // 1 object per question
   // question
